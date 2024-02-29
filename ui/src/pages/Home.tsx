d27e7c5ff2b16027053f66a37ebbfeb780c62dbd
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-02-29 18:42:26 +0200

    setup react charts

--- ui/src/pages/Home.tsx
+++ ui/src/pages/Home.tsx
@@ -1,6 +1,52 @@
 import { useState } from "react"
 import * as api from '../api'
 
+import React from "react";
+import { AxisOptions, Chart } from "react-charts";
+import useDemoConfig from "../hooks/useDemoConfig"
+
+function Line() {
+  const { data, randomizeData } = useDemoConfig({
+    series: 1,
+    dataType: "time",
+  });
+
+  const primaryAxis = React.useMemo<
+    AxisOptions<typeof data[number]["data"][number]>
+  >(
+    () => ({
+      getValue: (datum) => datum.primary as unknown as Date,
+    }),
+    []
+  );
+
+  const secondaryAxes = React.useMemo<
+    AxisOptions<typeof data[number]["data"][number]>[]
+  >(
+    () => [
+      {
+        getValue: (datum) => datum.secondary,
+      },
+    ],
+    []
+  );
+
+  return (
+    <>
+      <button onClick={randomizeData}>Randomize Data</button>
+      <br />
+      <br />
+        <Chart
+          options={{
+            data,
+            primaryAxis,
+            secondaryAxes,
+          }}
+        />
+    </>
+  );
+}
+
 export default function Home() {
     const [rows, setRows] = useState('')
     const [queryValue, setQueryValue] = useState('SELECT * FROM users')
@@ -9,6 +55,32 @@ export default function Home() {
         const data = await api.query(queryValue)
         setRows(JSON.stringify(data))
     }
+
+    const { data, randomizeData } = useDemoConfig({
+        series: 10,
+        dataType: "time",
+      });
+    
+      const primaryAxis = React.useMemo<
+        AxisOptions<typeof data[number]["data"][number]>
+      >(
+        () => ({
+          getValue: (datum) => datum.primary as unknown as Date,
+        }),
+        []
+      );
+    
+      const secondaryAxes = React.useMemo<
+        AxisOptions<typeof data[number]["data"][number]>[]
+      >(
+        () => [
+          {
+            getValue: (datum) => datum.secondary,
+          },
+        ],
+        []
+      );
+
     return (
         <div>
             <div className="flex items-center justify-center flex-col">
@@ -20,8 +92,16 @@ export default function Home() {
             <div className="flex items-center justify-center">
             <textarea value={rows} className="textarea-primary textarea-bordered w-[500px] h-[200px] p-2" readOnly={true}/>
             </div>
-
-            
+        <div className="w-[1000px] h-[500px]">
+        <Line />
+        </div>
         </div>
     )
 }
+
+
+
+
+
+
+
