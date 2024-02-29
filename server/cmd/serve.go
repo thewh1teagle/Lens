f85aa59671f5a9420ed142d78ddf3394589fac2a
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-03-01 01:41:07 +0200

    add more cmd args

--- server/cmd/serve.go
+++ server/cmd/serve.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/alecthomas/kingpin"
 	"github.com/gin-gonic/gin"
 	"github.com/thewh1teagle/lens/api"
@@ -12,11 +14,15 @@ import (
 var (
 	dbPath     = kingpin.Arg("db", "Path to db.").Required().String()
 	configPath = kingpin.Arg("config", "Path to config dashboard JSON.").Required().String()
+	port       = kingpin.Arg("port", "Port for the server").Default("8080").String()
+	host       = kingpin.Arg("host", "Host for the server").Default("127.0.0.1").String()
 )
 
 func main() {
 	kingpin.Parse()
+	gin.SetMode(gin.ReleaseMode) // hide default debug messages
 	r := gin.Default()
+
 	// Setup UI
 	ui.ServeUI(r)
 
@@ -24,7 +30,9 @@ func main() {
 	db, _ := db.New(*dbPath)
 	db.Connect()
 	api.Setup(r.Group("/api"), db, *configPath)
-	go schedule.Run(*configPath) // run scheduler in background
+	// Run scheduler in background
+	go schedule.Run(*configPath)
 	// Listen and Server in 0.0.0.0:8080
-	r.Run(":8080")
+	addr := fmt.Sprintf("%s:%s", *host, *port)
+	r.Run(addr)
 }
