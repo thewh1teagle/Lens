6d7bc7d28266ace252ace1485718274bdb6590c7
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-02-29 15:56:15 +0200

    setup tailwind and react router

---
+++ ui/src/pages/Home.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div>
+            <input className="input input-bordered"></input>
+            <button className="btn btn-primary">Hello</button>
+        </div>
+    )
+}
