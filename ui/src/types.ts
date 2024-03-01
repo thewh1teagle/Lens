f0386b64bc5c6edfc98308368c724438167b7333
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-03-01 04:31:56 +0200

    improve types and schema

--- ui/src/types.ts
+++ ui/src/types.ts
@@ -10,7 +10,7 @@ interface AxisConfig {
 }
 
 interface ItemConfig {
-    chart_type: string
+    chart_type: "line" | "area"
     x: AxisConfig
     y: AxisConfig
     query?: string
@@ -20,7 +20,7 @@ interface ItemConfig {
     debug?: boolean
     refresh_interval?: string
     date_range_format?: string
-    date_range?: string
+    date_range?: "last_1_minutes" | "last_5_minutes" | "last_1_hours" | "last_6_hours" | "last_1_day" | "last_1_week" | "last_1_month" | "last_1_year" | "last_10_years" | "today"
     user_agent?: string
 }
 
