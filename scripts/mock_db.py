fc44ebb1442fbbc00a0dea67d735838ba0f62044
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-03-03 00:54:13 +0200

    better mocking

--- scripts/mock_db.py
+++ scripts/mock_db.py
@@ -3,6 +3,7 @@ import sqlite3
 import sys
 from datetime import datetime, timedelta
 from pathlib import Path
+import time
 
 # Get the database file path from command line arguments
 path = Path(sys.argv[1])
@@ -25,44 +26,29 @@ def insert_data(speed, date):
     utc_date_str = date.isoformat() + "z"
     cursor.execute("INSERT INTO speedtest (speed, date) VALUES (?, ?)", (speed, utc_date_str))
 
-insert_data(0, datetime.utcnow())
-
-# # Generate and insert 1000 rows of data with random speed and date
-# for i in range(1000):
-#     # Generate random speed in the range [1, 100]
-#     speed = random.uniform(1, 100)
+def random_speed():
+    chance = random.randint(1, 100)
+    # 95% range(60,70)
+    if chance <= 95:
+        return random.randint(60, 70)
     
-#     # Generate date and time going back in time by one hour each time
-#     date = (datetime.utcnow() - timedelta(hours=i)).strftime('%Y-%m-%d %H:%M:%S')
-    
-#     # Insert the data into the table
-#     insert_data(speed, date)
+    # 2% range(30,59)
+    elif chance <= 98:
+        return random.randint(30, 59)
+    # 1% 0
+    else:
+        return 0
 
-# # Generate and insert data for today
-# today = datetime.utcnow().strftime('%Y-%m-%d')
-# for i in range(100):
-#     speed = random.uniform(1, 100)
-#     time = (datetime.strptime(today, '%Y-%m-%d') - timedelta(minutes=i)).strftime('%Y-%m-%d %H:%M:%S')
-#     insert_data(speed, time)
 
-# # Generate and insert data for last week
-# last_week = (datetime.utcnow() - timedelta(days=7)).strftime('%Y-%m-%d')
-# for i in range(100):
-#     speed = random.uniform(1, 100)
-#     time = (datetime.strptime(last_week, '%Y-%m-%d') - timedelta(minutes=i)).strftime('%Y-%m-%d %H:%M:%S')
-#     insert_data(speed, time)
+virtual_date = datetime.utcnow() - timedelta(days=30)
+while virtual_date < datetime.utcnow():
+    speed = random_speed()
+    print(f'Inserting date {virtual_date} with speed {speed}')
+    insert_data(speed, virtual_date)
+    virtual_date += timedelta(minutes=30)
+    # time.sleep(1)
 
-# # Generate and insert data for last year
-# last_year = (datetime.utcnow() - timedelta(days=365)).strftime('%Y-%m-%d')
-# for i in range(100):
-#     speed = random.uniform(1, 100)
-#     time = (datetime.strptime(last_year, '%Y-%m-%d') - timedelta(days=i)).strftime('%Y-%m-%d %H:%M:%S')
-#     insert_data(speed, time)
-
-# # Commit the changes to the database
 conn.commit()
-
-# # Close the connection
 conn.close()
 
 print("Database created successfully!")
